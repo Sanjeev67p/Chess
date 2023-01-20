9a9847db8c032a0d10c9b80f7eff9e3c4c3cc819
Sanjeev67p <sanjeevpatwal67@gmail.com>
2023-01-20 19:48:49 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let 
+let id0= document.querySelectorAll(".box")
+let pawn = document.querySelector(".fa-chess-pawn");
+
+pawn.addEventListener("click", oppawn);
+
+function oppawn(){
+    if(id0.id == "9"){
+        const element = document.getElementById("9")
+        element.remove();
+    }
+
+}
